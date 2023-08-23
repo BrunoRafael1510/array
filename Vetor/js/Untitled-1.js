676b62280015097ab8732fa772336bef7479e149
Bruno Rafael <140561029+BrunoRafael1510@users.noreply.github.com>
2023-08-22 22:22:04 -0300

    Add files via upload

---
+++ Vetor/js/Untitled-1.js
@@ -0,0 +1,47 @@
+// console.log("Olá Mundo")
+
+
+// let nota = 1;
+// let nota2 = 3;
+// let nota3 = 8.5;  
+let aluno = ["BruninGatao", "Murillo", "Biel", "Pedrin"]
+
+// console.log(aluno[2]); 
+
+// Metodo push() - Adiciona um novo elemento no final da matriz 
+//aluno.push("Senai")
+
+//Metodo unshift() - INtroduzir um novo elemento no inicio do array
+/* aluno.unshift("eu")
+aluno.unshift("eu1")
+aluno.unshift("eu2")
+aluno.unshift("eu3") */
+// Metodo pop() - que remove o ultimo
+// aluno.pop() - Ultimo elemento
+// aluno.shift() - Ultimo Elemento
+// console.table(aluno);
+
+// Metodo .length 
+// console.log("Tamanho da matriz : " + aluno.length)
+
+//console.table(aluno);
+//Inserir na posição um o elemento Maria Do Carmo
+// aluno[1] = "Maria Do Carmo"
+/* console.table(aluno)
+delete aluno[2]
+console.table(aluno) */
+
+let cadastro = [
+    ["Bruno", 9,10,5,5,8],
+    ["Josiane", 12,10,12,5],
+    ["Pedro",10, 10,8,5,9.5,10],
+    ["Ana", 5,5.5,6,7],
+    ["Claudia", 19,3.6,8.5,2.5]
+
+
+]   
+
+console.table(cadastro)
+console.log(cadastro[2][1]);
+
+
---
+++ Vetor/js/codigo.js
@@ -0,0 +1,4 @@
+/* 
+*NODE.JS - É um sofware de código aberto, multiplataforma
+* que paermite a execução de jasvascript fora do computador
+ */
